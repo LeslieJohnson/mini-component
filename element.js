6914df8f1ca6dfc394e18ca55bda4c3a3ea661b8
Leslie Johnson <bithits@gmail.com>
2021-11-20 14:41:47 +0000

    Add files via upload

---
+++ element.js
@@ -0,0 +1,15 @@
+export const element = (tag, attributes, ...children) => {
+  const el = document.createElement(tag)
+
+  for (let key in attributes) {
+    el.setAttribute(key, attributes[key])
+  }
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      el.appendChild(document.createTextNode(child))
+    } else {
+      el.appendChild(child)
+    }
+  })
+  return el
+}
